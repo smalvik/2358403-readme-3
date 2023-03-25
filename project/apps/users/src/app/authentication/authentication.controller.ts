567739f67d0db9f7296c2e4778ed4d6dbb4bc291
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-03-25 22:45:52 +0100

    Добавлен обработчик маршрута /register

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { fillObject } from '@project/util/util-core';
+import { UserRdo } from './rdo/user.rdo';
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
+
+  @Post('register')
+  public async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.authService.register(dto);
+    return fillObject(UserRdo, newUser);
+  }
 }
