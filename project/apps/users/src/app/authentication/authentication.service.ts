9e47ff4c8b7513dbb8a05110a3bdd6cbe197719f
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-03-25 21:18:46 +0100

    Добавлен метод для авторизации пользователя в AuthenticationService

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,10 +1,20 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { BlogUserMemoryRepository } from '../blog-user/blog-user-memory.repository';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserRole } from '@project/shared/app-types';
 import dayjs from 'dayjs';
-import { AUTH_USER_EXISTS } from './authentication.constant';
+import {
+  AUTH_USER_EXISTS,
+  AUTH_USER_NOT_FOUND,
+  AUTH_USER_PASSWORD_WRONG,
+} from './authentication.constant';
 import { BlogUserEntity } from '../blog-user/blog-user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthenticationService {
@@ -12,7 +22,6 @@ export class AuthenticationService {
 
   public async register(dto: CreateUserDto) {
     const { email, firstname, lastname, password, dateBirth } = dto;
-
     const blogUser = {
       email,
       firstname,
@@ -22,15 +31,31 @@ export class AuthenticationService {
       dateBirth: dayjs(dateBirth).toDate(),
       passwordHash: '',
     };
-
     const existUser = await this.blogUserRepository.findByEmail(email);
-
     if (existUser) {
       throw new ConflictException(AUTH_USER_EXISTS);
     }
-
     const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
-
     return this.blogUserRepository.create(userEntity);
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const { email, password } = dto;
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    const blogUserEntity = new BlogUserEntity(existUser);
+    if (!(await blogUserEntity.comparePassword(password))) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return blogUserEntity.toObject();
+  }
+
+  public async getUser(id: string) {
+    return this.blogUserRepository.findById(id);
   }
 }
