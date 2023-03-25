c395b4ce03a1cddf71797e800a434583eecefd84
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-03-25 22:55:41 +0100

    Добавлены обработчики маршрутов /login, /:id

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { fillObject } from '@project/util/util-core';
 import { UserRdo } from './rdo/user.rdo';
+import { LoggedUserRdo } from './rdo/logged-user.rdo';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthenticationController {
@@ -12,5 +14,17 @@ export class AuthenticationController {
   public async create(@Body() dto: CreateUserDto) {
     const newUser = await this.authService.register(dto);
     return fillObject(UserRdo, newUser);
+  }
+
+  @Post('login')
+  public async login(@Body() dto: LoginUserDto) {
+    const verifiedUser = await this.authService.verifyUser(dto);
+    return fillObject(LoggedUserRdo, verifiedUser);
+  }
+
+  @Get(':id')
+  public async show(@Param('id') id: string) {
+    const existUser = await this.authService.getUser(id);
+    return fillObject(UserRdo, existUser);
   }
 }
