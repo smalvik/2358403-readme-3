fbb1313f152e0cdd6e6543cd8971975231868876
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-03-31 23:06:59 +0100

    Добавлено описание для RDO (ApiResponse)

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -5,7 +5,7 @@ import { fillObject } from '@project/util/util-core';
 import { UserRdo } from './rdo/user.rdo';
 import { LoggedUserRdo } from './rdo/logged-user.rdo';
 import { LoginUserDto } from './dto/login-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('authentication')
 @Controller('auth')
@@ -18,12 +18,18 @@ export class AuthenticationController {
     return fillObject(UserRdo, newUser);
   }
 
+  @ApiResponse({
+    type: LoggedUserRdo,
+  })
   @Post('login')
   public async login(@Body() dto: LoginUserDto) {
     const verifiedUser = await this.authService.verifyUser(dto);
     return fillObject(LoggedUserRdo, verifiedUser);
   }
 
+  @ApiResponse({
+    type: UserRdo,
+  })
   @Get(':id')
   public async show(@Param('id') id: string) {
     const existUser = await this.authService.getUser(id);
