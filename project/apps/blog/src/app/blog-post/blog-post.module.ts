9e793e84f283564faf2b9b0ec403831a1487456b
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-04-02 15:37:08 +0100

    В контроллер BlogPostController добавлен сервис BlogPostService

--- project/apps/blog/src/app/blog-post/blog-post.module.ts
+++ project/apps/blog/src/app/blog-post/blog-post.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BlogPostMemoryRepository } from './blog-post-memory.repository';
 
-// import { BlogPostController } from './blog-post.controller';
+import { BlogPostController } from './blog-post.controller';
 import { BlogPostService } from './blog-post.service';
 
 @Module({
-  // controllers: [],
+  controllers: [BlogPostController],
   providers: [BlogPostMemoryRepository, BlogPostService],
   exports: [BlogPostMemoryRepository],
 })
