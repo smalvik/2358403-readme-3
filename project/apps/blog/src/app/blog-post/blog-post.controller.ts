c07cee8a07d75033e281dc343d1c854e09abb852
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-04-02 19:39:26 +0100

    Добавлен обработчик create в контроллер BlogPostController

--- project/apps/blog/src/app/blog-post/blog-post.controller.ts
+++ project/apps/blog/src/app/blog-post/blog-post.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { BlogPostService } from './blog-post.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { BlogPostRdo } from './rdo/blog-post.rdo';
+import { fillObject } from '@project/util/util-core';
 
 @Controller('blog-post')
-export class BlogPostController {}
+export class BlogPostController {
+  constructor(private readonly blogPostService: BlogPostService) {}
+
+  @Post('create')
+  public async create(@Body() dto: CreatePostDto) {
+    const newPost = await this.blogPostService.create(dto);
+    return fillObject(BlogPostRdo, newPost);
+  }
+}
