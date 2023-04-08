435e8179d71affe10b6df440dd2bddc0fcd0767e
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-04-08 14:41:28 +0100

    Добавлены все виды DTO (video, text, quote, photo, link) для публикаций

--- project/apps/blog/src/app/blog-post/dto/create-post.dto.ts
+++ project/apps/blog/src/app/blog-post/dto/create-post.dto.ts
@@ -1,7 +1,40 @@
 import { PostTypeEnum, PostStatusEnum } from '@project/shared/app-types';
 
-export class CreatePostDto {
+export class CreatePostBaseDto {
   public type: PostTypeEnum;
   public status: PostStatusEnum;
   public tags?: string[];
 }
+
+export class CreatePostVideoDto extends CreatePostBaseDto {
+  public title: string;
+  public link: string;
+}
+
+export class CreatePostTextDto extends CreatePostBaseDto {
+  public title: string;
+  public announcement: string;
+  public text: string;
+}
+
+export class CreatePostQuoteDto extends CreatePostBaseDto {
+  public quote: string;
+  public author: string;
+}
+
+export class CreatePostPhotoDto extends CreatePostBaseDto {
+  public photo: string;
+}
+
+export class CreatePostLinkDto extends CreatePostBaseDto {
+  public link: string;
+  public description?: string;
+}
+
+export type CreatePostDto =
+  | CreatePostBaseDto
+  | CreatePostVideoDto
+  | CreatePostTextDto
+  | CreatePostQuoteDto
+  | CreatePostPhotoDto
+  | CreatePostLinkDto;
