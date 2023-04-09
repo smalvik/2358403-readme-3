8a0671740a618893bae2bc30cdc9bd21a04bc898
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-04-09 20:46:50 +0100

    Добавлено DTO BlogPostCommentDto, и реализован метод create в BlogPostCommentService

--- project/apps/blog/src/app/blog-post-comment/blog-post-comment.service.ts
+++ project/apps/blog/src/app/blog-post-comment/blog-post-comment.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { BlogPostCommentMemoryRepository } from './blog-post-comment-memory.repository';
+import { CreatePostCommentDto } from './dto/create-post-comment.dto';
+import { CommentStatusEnum } from '@project/shared/app-types';
+import { BlogPostCommentEntity } from './blog-post-comment.entity';
 
 @Injectable()
 export class BlogPostCommentService {
   constructor(
     private readonly blogPostCommentRepository: BlogPostCommentMemoryRepository
   ) {}
+
+  public async register(dto: CreatePostCommentDto) {
+    const { userId, postId, text } = dto;
+
+    const blogPostComment = {
+      userId,
+      postId,
+      text,
+      status: CommentStatusEnum.Moderation,
+      creationDate: Date.now(),
+      publicationDate: Date.now(),
+    };
+
+    const blogPostEntity = new BlogPostCommentEntity(blogPostComment);
+
+    return this.blogPostCommentRepository.create(blogPostEntity);
+  }
 }
---
+++ project/apps/blog/src/app/blog-post-comment/dto/create-post-comment.dto.ts
@@ -0,0 +1,5 @@
+export class CreatePostCommentDto {
+  public userId: string;
+  public postId: string;
+  public text: string;
+}
