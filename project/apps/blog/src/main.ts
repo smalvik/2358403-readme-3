8eb47df26086af4595573579e208fb1889e5c02b
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-04-24 17:46:54 +0100

    Добавлена базовая настройка @nestjs/swagger для микросервиса Blog

--- project/apps/blog/src/main.ts
+++ project/apps/blog/src/main.ts
@@ -7,11 +7,23 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('The «Blog» service')
+    .setDescription('Blog service API')
+    .setVersion('1.0')
+    .build();
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
