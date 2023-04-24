0b3cf80f383cd65255bd875f482e443ef9b3dd47
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-04-24 19:39:31 +0100

    Добавлено описание для CreatePostBaseDto (@ApiProperty)

--- project/apps/blog/src/app/blog-post/dto/create-post.dto/create-post-base.dto.ts
+++ project/apps/blog/src/app/blog-post/dto/create-post.dto/create-post-base.dto.ts
@@ -1,7 +1,22 @@
 // import { PostTypeEnum, PostStatusEnum } from '@project/shared/app-types';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePostBaseDto {
+  @ApiProperty({
+    description: 'Post type',
+    example: 'video',
+  })
   public type: string;
+
+  @ApiProperty({
+    description: 'Post status',
+    example: 'draft',
+  })
   public status: string;
+
+  @ApiProperty({
+    description: 'Post tags',
+    example: 'tag1',
+  })
   public tags?: string[];
 }
